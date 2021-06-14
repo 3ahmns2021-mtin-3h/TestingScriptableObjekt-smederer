4a383aa97e87cd7b9165f76f7798ca85700c19b7
Sebastian Mederer <sebastian.mederer03@htl-salzburg.ac.at>
2021-06-14 13:28:02 +0200

    SO erstellt und wird verwendet

---
+++ Assets/MyGame/Scripts/Car.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "CarStuff")]
+public class Car : ScriptableObject
+{
+    
+    public string Marke;
+    public string Frabe;
+    public int Kosten;
+    
+
+}
+
+
---
+++ Assets/MyGame/Scripts/CarEigenschaften.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarEigenschaften : MonoBehaviour
+{
+    
+    public Car car;
+    void Start()
+    {
+        Debug.Log(car.Marke);
+    }
+
+    
+}
